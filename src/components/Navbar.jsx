a40c144eb62f8ea5cbba66d5aba315b7d6097f26
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2024-07-12 14:32:54 +0200

    add navbar for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import "../styles/navbar.css";
+import LinkedInIcon from "../assets/linkedin-icon.svg?react";
+import  GithubIcon from "../assets/github-icon.svg?react";
 
 const Navbar = () => {
   return (
     <nav>
         <h1>Frederik Skrubbeltrang</h1>
+        <div className="flex-container">
+          <a href="https://www.linkedin.com/in/frederik-skrubbeltrang-698b4a1a3/" target="_blank"><LinkedInIcon/></a>
+          <a href="https://github.com/fredeskrubba" target="_blank"><GithubIcon/> </a>
+        </div>
     </nav>
   )
 }
