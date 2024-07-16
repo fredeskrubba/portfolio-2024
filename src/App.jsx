27e03a9c6acd6f949964e5e973e993511f229f5e
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2024-07-16 19:52:09 +0200

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import '../src/styles/App.css'
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
+import About from './components/About'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Intro/>
+      <About/>
     </>
   )
 }
