c167ff536813d769597bdc74c13761e497aff0a1
Frederik Skrubbeltrang <frederikskrubbeltrangbs@gmail.com>
2024-08-02 16:59:26 +0200

    add responsitivity

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -26,25 +26,30 @@ const Projects = () => {
     ]
 
     return ( 
-        <section id="projects-section">
-            <div className="section-collapsable">
-                <h2>Projekter</h2>
-                <ArrowIcon className={showSection ? "" : "rotate-arrow"} onClick={()=> {
-                    setShowSection(!showSection)
-                }}/>
-            </div>
-            {
-                showSection &&
-                    <>
-                        <article id="projects-container">
-                            {projectsArray.map((project) => <Project title={project.title} description={project.description} tags={project.tags} githubLink={project.githubLink} demoLink={project.demoLink} image={project.image} key={project.id}/>)}
-                        </article>
-                        <button type="button" className="see-more">
-                            Se mere
-                        </button>
-                    </>
-            }
-        </section>
+        <>
+            <section id="projects-section-mobile">
+                <div className="section-collapsable">
+                    <h2>Projekter</h2>
+                    <ArrowIcon className={showSection ? "" : "rotate-arrow"} onClick={()=> {
+                        setShowSection(!showSection)
+                    }}/>
+                </div>
+                {
+                    showSection &&
+                        <>
+                            <article id="projects-container">
+                                {projectsArray.map((project) => <Project title={project.title} description={project.description} tags={project.tags} githubLink={project.githubLink} demoLink={project.demoLink} image={project.image} key={project.id}/>)}
+                            </article>
+                            <button type="button" className="see-more">
+                                Se mere
+                            </button>
+                        </>
+                }
+            </section>
+            <section id="projects-section-tablet">
+
+            </section>
+        </>
     );
 }
  
