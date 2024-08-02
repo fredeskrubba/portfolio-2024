daa289868852c55c1d85ce56e183e884cb1ca3b9
Frederik Skrubbeltrang <frederikskrubbeltrangbs@gmail.com>
2024-08-02 17:07:06 +0200

    Revert "add responsitivity"
    This reverts commit c167ff536813d769597bdc74c13761e497aff0a1.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,15 +4,10 @@ import IMAGES from "../assets/images/Images"
 const Intro = () => {
   return (
     <header>
-      <div className="intro-mobile">
         <img src={IMAGES.profilePicture} alt="Picture of me" className="intro-image"/>
         <h2>
             I am [Your Name], a web developer passionate about creating dynamic, user-friendly websites. I specialize in clean code and seamless user experiences, delivering high-quality solutions for your online presence.
         </h2>
-      </div>
-      <div className="intro-tablet">
-        
-      </div>
     </header>
   )
 }
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -30,30 +30,30 @@ const About = () => {
     const [showSection, setShowSection] = useState(true)
 
     return (
-            <section id="about-section-mobile">
-                <div className="section-collapsable">
-                    <h2>Om mig</h2>
-                    <ArrowIcon className={showSection ? "" : "rotate-arrow"} onClick={()=> {
-                        setShowSection(!showSection)
-                    }}/>
-                </div>
-                {
-                    showSection && 
-                    <>
-                        <article className="section-intro">
-                            <p>I en tidsalder hvor få ting er så vigtige, som en stærk online tilstedeværelse, er dygtige og engagerede udviklere vigtigere end nogensinde før. Som uddannet webudvikler og datamatiker, med tre års erhvervserfaring har jeg kastet mig ind i kampen med hovedet først. I mit repetoir kan man blandt andet finde følgende:</p>
-                        </article>
-                        <article id="skills-container">
-                            {
-                                skills.map((skill)=><Skill title={skill.title} Icon={skill.icon} key={skill.id}/>)
-                            }
-                        </article>
-                        <button type="button" className="see-more">
-                            Se mere
-                        </button>                
-                    </>
-                }
-            </section>
+        <section id="about-section">
+            <div className="section-collapsable">
+                <h2>Om mig</h2>
+                <ArrowIcon className={showSection ? "" : "rotate-arrow"} onClick={()=> {
+                    setShowSection(!showSection)
+                }}/>
+            </div>
+            {
+                showSection && 
+                <>
+                    <article className="section-intro">
+                        <p>I en tidsalder hvor få ting er så vigtige, som en stærk online tilstedeværelse, er dygtige og engagerede udviklere vigtigere end nogensinde før. Som uddannet webudvikler og datamatiker, med tre års erhvervserfaring har jeg kastet mig ind i kampen med hovedet først. I mit repetoir kan man blandt andet finde følgende:</p>
+                    </article>
+                    <article id="skills-container">
+                        {
+                            skills.map((skill)=><Skill title={skill.title} Icon={skill.icon} key={skill.id}/>)
+                        }
+                    </article>
+                    <button type="button" className="see-more">
+                        Se mere
+                    </button>                
+                </>
+            }
+        </section>
     )
 }
 
